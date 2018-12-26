966a13efcad99dd6c417073dd91a27799b850c69
Kuang Yu <yu.kuang@sz.tsinghua.edu.cn>
2018-12-26 20:32:03 +0800

    Finish construct_neighbour_list

--- lib/neighbour_list.cpp
+++ lib/neighbour_list.cpp
@@ -32,10 +32,10 @@ using namespace Eigen;
 // n_atoms: integer
 // box, box_inv: box dimensions and its inversion
 // r_cut: realspace cutoff distance, in Angstrom
-Tensor<int, 4> construct_cell_list(const MatrixXd & positions, const ArrayXi & atom_labels,
+Tensor<int, 4, RowMajor> construct_cell_list(const MatrixXd & positions, const ArrayXi & atom_labels,
     int n_atoms, const MatrixXd & box, const MatrixXd & box_inv, double r_cut)
 {
-  Tensor<int, 4> celllist;
+  Tensor<int, 4, RowMajor> celllist;
   double vol = volume(box);
   double ab, bc, ca;
   int na, nb, nc, n_max;
@@ -64,10 +64,31 @@ Tensor<int, 4> construct_cell_list(const MatrixXd & positions, const ArrayXi & a
   celllist.resize(na, nb, nc, n_max);
   celllist.setConstant(-1);
 
-  matrix 
-  for (int i_atom=0; i_atom<n_atoms, i_atom++) {
-    label = atom_labels[i_atom];
+  MatrixXd cell=box, cell_inv;
+  cell.row(0) /= na;
+  cell.row(1) /= nb;
+  cell.row(2) /= nc;
+  cell_inv = cell.inverse();
 
+  // cell indices of each particle
+  MatrixXd spositions = positions * cell_inv;
+  MatrixXi indices = spositions.array().floor().cast<int>();
+  for (int i_atom=0; i_atom<n_atoms; i_atom++) {
+    // pbc shifts
+    int label = atom_labels(i_atom);
+    int ia = indices(i_atom, 0) % na;
+    int ib = indices(i_atom, 1) % nb;
+    int ic = indices(i_atom, 2) % nc;
+    if (indices(i_atom, 0) < 0) ia += na;
+    if (indices(i_atom, 1) < 0) ib += nb;
+    if (indices(i_atom, 2) < 0) ic += nc;
+    // assign atom to a particular cell
+    for (int i=0; i<n_max; i++) {
+      if (celllist(ia, ib, ic, i) < 0) {
+        celllist(ia, ib, ic, i) = label;
+        break;
+      }
+    }
   }
 
   return celllist;
@@ -77,7 +98,7 @@ Tensor<int, 4> construct_cell_list(const MatrixXd & positions, const ArrayXi & a
 ArrayXi construct_cell_list_pywrap(const MatrixXd & positions, const ArrayXi & atom_labels,
     int n_atoms, const MatrixXd & box, const MatrixXd & box_inv, double r_cut)
 {
-  Tensor<int, 4> celllist;
+  Tensor<int, 4, RowMajor> celllist;
   celllist = construct_cell_list(positions, atom_labels, n_atoms, box, box_inv, r_cut);
   ArrayXi results;
   Map<ArrayXi> data(celllist.data(), celllist.size());
@@ -87,7 +108,7 @@ ArrayXi construct_cell_list_pywrap(const MatrixXd & positions, const ArrayXi & a
   results(1) = celllist.dimension(1);
   results(2) = celllist.dimension(2);
   results(3) = celllist.dimension(3);
-  results.segment(4, n1*n2*n3*n4) = data;
+  results.segment(4, celllist.size()) = data;
   return results;
 }
 
